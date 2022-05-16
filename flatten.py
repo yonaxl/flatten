1789169c614d28a262e88d20876a51548a477c81
Yonax Lonard <69830097+yonaxl@users.noreply.github.com>
2022-05-16 13:41:44 +1000

    Add files via upload
    My version of bug fix.

--- flatten.py
+++ flatten.py
@@ -1,9 +1,9 @@
-
 def flatten(arr):
+    to_return = []
     for x in arr:
         if isinstance(x, list):
             for y in flatten(x):
-                yield y
+                to_return.append(y)
         else:
-            yield x
-
+            to_return.append(x)
+    return to_return
